2ca3e9b1eb34b0781a4cbbe99b8e96855a7a2e33
Antonin LYAËT <31038778+syukoGit@users.noreply.github.com>
2025-07-21 10:00:48 +0200

    Add event creation flow with Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import Logo from "/logo.png";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import EventConfig from './pages/EventConfig';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <img src={Logo} className="App-logo" alt="logo" />
-      <div>
-        <h1>Cochonnet-18</h1>
-        <p>Logiciel de gestion de tournoi de pétanque</p>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/config" element={<EventConfig />} />
+      </Routes>
+    </Router>
   );
 }
 
