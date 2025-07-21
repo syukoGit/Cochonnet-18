29d03c081268a9bedbf7fc0a601da070ed921331
Antonin LYAËT <syukodev@outlook.com>
2025-07-21 16:18:32 +0200

    Refactor EventConfig component to use FloatingLabelInput for improved UI consistency and accessibility

--- src/pages/EventConfig.tsx
+++ src/pages/EventConfig.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { addTeam, setMatches, setName } from '../store/eventSlice';
 import './EventConfig.css';
+import FloatingLabelInput from '../components/FloatingLabelInput';
 
 function EventConfig() {
   const dispatch = useAppDispatch();
@@ -17,49 +18,48 @@ function EventConfig() {
 
   return (
     <div className="config-wrapper">
+      <h1 className="config-title">Configuration de l'évènement</h1>
       <div className="config-container">
         <div className="left-panel">
-          <div className="field">
-            <label>Nom de l'évènement</label>
-            <input
-              type="text"
-              value={name}
-              placeholder="Nom de l'évènement"
-              onChange={(e) => dispatch(setName(e.target.value))}
-            />
-          </div>
-          <div className="field">
-            <label>Nombre de matchs par équipe</label>
-            <input
-              type="number"
-              min={1}
-              value={matches}
-              placeholder="Nombre de matchs"
-              onChange={(e) => dispatch(setMatches(Number(e.target.value)))}
-            />
-          </div>
+          <FloatingLabelInput
+            id="event-name"
+            label="Nom de l'évènement"
+            value={name}
+            onChange={(e) => dispatch(setName(e.target.value))}
+          />
+          <FloatingLabelInput
+            id="nb-matches"
+            label="Nombre de matchs"
+            type="number"
+            value={matches.toString()}
+            onChange={(e) => dispatch(setMatches(Number(e.target.value)))}
+          />
         </div>
-        <div className="right-panel">
-          <div className="team-header">{teams.length} équipes</div>
-          <div className="team-list-container">
-            <ul className="team-list">
-              {teams.map((team) => (
-                <li key={team}>{team}</li>
-              ))}
-            </ul>
-          </div>
-          <div className="team-add">
-            <input
-              type="text"
-              value={newTeam}
-              placeholder="Nom de l'équipe"
-              onChange={(e) => setNewTeam(e.target.value)}
-            />
-            <button onClick={handleAddTeam}>Ajouter</button>
-          </div>
+        <h4 className="team-list-header">{teams.length} équipes</h4>
+        <div className="team-list-container">
+          <ul className="team-list">
+            {teams.map((team) => (
+              <li key={team}>{team}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="team-add">
+          <FloatingLabelInput
+            id="team-name"
+            label="Ajouter une équipe"
+            value={newTeam}
+            onChange={(e) => setNewTeam(e.target.value)}
+            type="text"
+            name="team-name"
+          />
+          <button type="button" onClick={handleAddTeam}>
+            Ajouter
+          </button>
         </div>
       </div>
-      <button className="start-button">Démarrer</button>
+      <button className="start-button" type="button">
+        Démarrer
+      </button>
     </div>
   );
 }
